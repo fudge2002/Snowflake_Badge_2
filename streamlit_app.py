74057ea9a02aff3f0a48392434beec05eb657348
fudge2002 <44732042+fudge2002@users.noreply.github.com>
2026-02-18 11:18:09 +0000

    Fix variable assignment for Smoothiefroot response

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,7 +102,7 @@ st.dataframe(
 )
 #new sectin api
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json(),use_container_width=true)
+sf_df = st.text(smoothiefroot_response.json(),use_container_width=true)
 
 
 
